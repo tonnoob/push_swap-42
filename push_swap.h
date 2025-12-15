92fec0c398a1238a44f4e7167502eeb069219523
Tonnoob <ottonsouzanto@gmail.com>
2025-12-15 02:48:30 -0300

    Making structs and understanding moves in stack

---
+++ push_swap.h
@@ -0,0 +1,25 @@
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include "libftprintf/ft_printf/ft_printf.h"
+# include "libftprintf/libft/libft.h"
+
+typedef struct node_s 
+{
+	int		n;
+	node_t	*next;
+} node_t;
+
+typedef struct stack_s
+{
+	node_t	*head;
+	int		size;
+} stack_t;
+
+// sa / sb: trocam os dois primeiros nós
+// pa / pb: movem o primeiro nó de uma stack para a outra
+// ra / rb: movem o primeiro nó para o final
+// rra / rrb: movem o último nó para o topo
+
+
+#endif
